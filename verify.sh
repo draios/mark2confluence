86eb663126e82e22cd65efcc2b3bc11921dc7907
vcaldaralo <vito.caldaralo@sysdig.com>
2021-11-22 12:54:26 +0100

    fix dry-run and verify

--- verify.sh
+++ verify.sh
@@ -5,6 +5,6 @@ find . -type d -not -path '**/\.*' -path "./${DOC_DIR_PATTERN}" |
     while read -r doc_dir; do
         echo "==> Verify markdown files into ${doc_dir}"
         pushd "${doc_dir}"
-        grep -R -l 'Space:' *.md | xargs -n1 -I{} mark -p "${CONFLUENCE_PASSWORD}" -u "${CONFLUENCE_USERNAME}" -b "${BASE_URL}" --debug --compile-only -f {} > /dev/null
+        grep -R -l 'Space:' $(find . -type f -name '*.md' -mmin -30) | xargs -n1 -I{} mark -p "${CONFLUENCE_PASSWORD}" -u "${CONFLUENCE_USERNAME}" -b "${BASE_URL}" --debug --compile-only -f {} > /dev/null
         popd
     done
