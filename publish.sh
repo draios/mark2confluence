e73e3dc65882d212c5f306ace4d3ae2b1e8d6887
vcaldaralo <vito.caldaralo@sysdig.com>
2022-01-05 17:56:28 +0100

    rollback

--- publish.sh
+++ publish.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -l
 set -eo pipefail
 
-HEADER_TPL="---\n\n**WARNING**: This page is automatically generated from [this source code]({{ .source_link}})\n\n---"
-HEADER_INCLUDE="<!-- Include: ./header.md\n\tsource_link: \"SOURCE_LINK\" -->"
+HEADER_TPL="---\n\n**WARNING**: This page is automatically generated from [this source code](SOURCE_LINK)\n\n---\n"
 
 find . -type d -not -path '**/\.*' -path "./${DOC_DIR_PATTERN}" |
     while read -r doc_dir; do
@@ -11,14 +10,11 @@ find . -type d -not -path '**/\.*' -path "./${DOC_DIR_PATTERN}" |
         pushd "${doc_dir}"
         grep -R -l 'Space:' *.md | 
             while read -r md_file; do
-                echo ${HEADER_TPL} > header.md
-                cat header.md
                 source_link=${source_dir}/${md_file}
                 echo "==> Verify markdown file ${source_link}"
-                header=${HEADER_INCLUDE/SOURCE_LINK/$source_link}
-                awk -v f="$header" '/Title/{print; print f; next}1' ${md_file} > ${md_file}-header.md
-                cat ${md_file}-header.md
-                mark -p "${CONFLUENCE_PASSWORD}" -u "${CONFLUENCE_USERNAME}" -b "${BASE_URL}" --debug -f ${md_file}-header.md > /dev/null
+                header=${HEADER_TPL/SOURCE_LINK/$source_link}
+                awk -v f="$header" '/Title/{print; print f; next}1' ${md_file} > /tmp/${md_file}
+                mark -p "${CONFLUENCE_PASSWORD}" -u "${CONFLUENCE_USERNAME}" -b "${BASE_URL}" --debug -f /tmp/${md_file} > /dev/null
             done
         popd
     done
