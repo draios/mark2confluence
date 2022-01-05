0909e8cc7f3bfe3ed17c510f4f6916c2d544e85b
vcaldaralo <vito.caldaralo@sysdig.com>
2022-01-05 17:38:07 +0100

    adding debug line

--- publish.sh
+++ publish.sh
@@ -12,6 +12,7 @@ find . -type d -not -path '**/\.*' -path "./${DOC_DIR_PATTERN}" |
         grep -R -l 'Space:' *.md | 
             while read -r md_file; do
                 echo ${HEADER_TPL} > ${doc_dir}/header.md
+                ls ${doc_dir}
                 source_link=${source_dir}/${md_file}
                 echo "==> Verify markdown file ${source_dir}/${md_file} (${source_link})"
                 header=${HEADER_INCLUDE/SOURCE_LINK/$source_link}
