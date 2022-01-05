d8531120329985106c63edcea45516d9f6963d1e
vcaldaralo <vito.caldaralo@sysdig.com>
2022-01-05 17:43:21 +0100

    minor fix

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 HEADER_TPL="\n---\n\n**WARNING**: This page is automatically generated from [this source code]({{ .source_link}})\n\n---\n"
-HEADER_INCLUDE='\n<!-- Include: ./header.md source_link: "SOURCE_LINK" -->\n'
+HEADER_INCLUDE='<!-- Include: ./header.md source_link: "SOURCE_LINK" -->'
 
 find . -type d -not -path '**/\.*' -path "./${DOC_DIR_PATTERN}" |
     while read -r doc_dir; do
@@ -11,7 +11,7 @@ find . -type d -not -path '**/\.*' -path "./${DOC_DIR_PATTERN}" |
         pushd "${doc_dir}"
         grep -R -l 'Space:' *.md | 
             while read -r md_file; do
-                echo ${HEADER_TPL} > ./header.md
+                echo ${HEADER_TPL} > header.md
                 source_link=${source_dir}/${md_file}
                 echo "==> Verify markdown file ${source_link}"
                 header=${HEADER_INCLUDE/SOURCE_LINK/$source_link}
