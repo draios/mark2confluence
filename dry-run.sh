acb715338db5b1ad1496411be38a7bbe1817f258
vcaldaralo <vito.caldaralo@sysdig.com>
2022-01-05 17:10:45 +0100

    fix dry-run and verify with header

--- dry-run.sh
+++ dry-run.sh
@@ -1,20 +1,19 @@
 #!/bin/bash -l
 set -eo pipefail
 
-echo "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/blob/${github.ref}"
+HEADER_TPL="---\n\n**WARNING**: This page is automatically generated from [this source code](SOURCE_LINK)\n\n---\n"
 
 find . -type d -not -path '**/\.*' -path "./${DOC_DIR_PATTERN}" |
     while read -r doc_dir; do
-        echo "${doc_dir:2}"
-        source_dir=${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/blob/${{ github.ref }}/${doc_dir:2}
-        echo "==> Verify markdown files into ${source_dir}"
-        pushd "${doc_dir}" 
+        source_dir=${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/blob/${GITHUB_REF_NAME}/${doc_dir:2}
+        echo "==> Upload markdown files into ${source_dir}"
+        pushd "${doc_dir}"
         grep -R -l 'Space:' *.md | 
             while read -r md_file; do
                 source_link=${source_dir}/${md_file}
                 echo "==> Verify markdown file ${source_link}"
-                sed -e "s|SOURCE_LINK|${source_link}|" header.md.tpl > /tmp/header.md
-                sed -e "/Title/r /tmp/header.md" ${md_file} > /tmp/${md_file}
+                header=${HEADER_TPL/SOURCE_LINK/$source_link}
+                awk -v f="$header" '/Title/{print; print f; next}1' ${md_file} > /tmp/${md_file}
                 mark -p "${CONFLUENCE_PASSWORD}" -u "${CONFLUENCE_USERNAME}" -b "${BASE_URL}" --debug --dry-run -f /tmp/${md_file} > /dev/null
             done
         popd
--- publish.sh
+++ publish.sh
@@ -8,13 +8,12 @@ find . -type d -not -path '**/\.*' -path "./${DOC_DIR_PATTERN}" |
         source_dir=${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/blob/${GITHUB_REF_NAME}/${doc_dir:2}
         echo "==> Upload markdown files into ${source_dir}"
         pushd "${doc_dir}"
-        #grep -R -l 'Space:' *.md | xargs -n1 -I{} mark -p "${CONFLUENCE_PASSWORD}" -u "${CONFLUENCE_USERNAME}" -b "${BASE_URL}" --debug -f {} > /dev/null
         grep -R -l 'Space:' *.md | 
             while read -r md_file; do
                 source_link=${source_dir}/${md_file}
                 echo "==> Verify markdown file ${source_link}"
                 header=${HEADER_TPL/SOURCE_LINK/$source_link}
-                awk -v f="$header" '/Layout/{print; print f; next}1' ${md_file} > /tmp/${md_file}
+                awk -v f="$header" '/Title/{print; print f; next}1' ${md_file} > /tmp/${md_file}
                 mark -p "${CONFLUENCE_PASSWORD}" -u "${CONFLUENCE_USERNAME}" -b "${BASE_URL}" --debug -f /tmp/${md_file} > /dev/null
             done
         popd
