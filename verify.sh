d3e727a54e99e35dfb831d62168a85117c88fde7
vcaldaralo <63840761+vcaldaralo@users.noreply.github.com>
2022-01-26 16:48:20 +0100

    DEVOPS-7325 Auto-header (#3)
    * testing automatic header
    
    * first version auto header
    
    * testing ${{ github.head_ref }}
    
    * tmp md_file
    
    * testing github.ref
    
    * fix mark target file
    
    * testing auto-header
    
    * debug
    
    * fix
    
    * using GITHUB_REF
    
    * removing unused doc_dir there
    
    * using GITHUB_REF_NAME
    
    * using header as variable
    
    * echo var not cat
    
    * using echo to concat files
    
    * using aws instead of sed to insert header
    
    * using simple concat
    
    * using awk
    
    * another \n
    
    * testing after Layout
    
    * fix dry-run and verify with header
    
    * debug
    
    * testing after layout
    
    * testing include
    
    * adding debug line
    
    * fix
    
    * minor fix
    
    * include new line
    
    * fix HEADER_INCLUDE
    
    * this dir
    
    * different file
    
    * rollback
    
    * removing unused test file
    
    * testing publish file modified less then 30min
    
    * variable default values
    
    * shellcheck exeption
    
    * fixing whitespace and eof

--- verify.sh
+++ verify.sh
@@ -1,13 +1,23 @@
 #!/bin/bash -l
 set -eo pipefail
 
-find . -type d -not -path '**/\.*' -path "./${DOC_DIR_PATTERN}" |
+HEADER_TPL="---\n\n**WARNING**: This page is automatically generated from [this source code](SOURCE_LINK)\n\n---\n"
+
+find . -type d -not -path '**/\.*' -path "./${DOC_DIR_PATTERN:-*}" |
     while read -r doc_dir; do
-        echo "==> Verify markdown files into ${doc_dir}"
+        source_dir=${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/blob/${GITHUB_REF_NAME}/${doc_dir:2}
+        echo "==> Upload markdown files into ${source_dir}"
         pushd "${doc_dir}"
         count=$(find . -type f -name "*.md" | wc -l | tr -d ' ')
         if [[ "${count}" -gt 0 ]]; then
-            grep -R -l 'Space:' $(find . -type f -name '*.md' -mmin -30) | xargs -n1 -I{} mark -p "${CONFLUENCE_PASSWORD}" -u "${CONFLUENCE_USERNAME}" -b "${BASE_URL}" --debug --compile-only -f {} > /dev/null
+            grep -R -l 'Space:' *.md |
+                while read -r md_file; do
+                    source_link=${source_dir}/${md_file}
+                    echo "==> Verify markdown file ${source_link}"
+                    header=${HEADER_TPL/SOURCE_LINK/$source_link}
+                    awk -v f="$header" '/Title/{print; print f; next}1' ${md_file} > /tmp/${md_file}
+                    mark -p "${CONFLUENCE_PASSWORD}" -u "${CONFLUENCE_USERNAME}" -b "${BASE_URL}" --debug --compile-only -f /tmp/${md_file} > /dev/null
+                done
         else
             echo "==> No *.md file found, skipping directory"
         fi
